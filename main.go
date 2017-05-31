47b1e5c742f36cbcbe5c8f91d92b3ed430ba4447
Rinzen Necroforger <Necroforger@gmail.com>
2017-05-31 01:51:07 -0400

    Add disabled commands to default config

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ func main() {
 					Selfbot: false,
 					Prefix:  "!",
 				},
+				DisabledCommands: []string{},
 			})
 			if err != nil {
 				log.Println("Error saving the configuration file: ", err)
