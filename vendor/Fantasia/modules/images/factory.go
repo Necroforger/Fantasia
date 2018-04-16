0ff948a998dddd1c1237e920ddcae2a4c3020f0d
necro <necroforger@gmail.com>
2018-04-16 09:59:03 -0400

    Allow min and max constraints to be set on factory functions

--- vendor/Fantasia/modules/images/factory.go
+++ vendor/Fantasia/modules/images/factory.go
@@ -23,8 +23,15 @@ func (m *Module) NewEffectCmdSingle(fn func(image.Image) *image.RGBA) func(ctx *
 	}
 }
 
+// EffectOptions are Effect options to be passed to the
+// Effect creation commands.
+type EffectOptions struct {
+	Max, Min                   float64
+	ConstrainMax, ConstrainMin bool
+}
+
 // NewEffectCommandFloat produces an effect command that accepts an image and a float
-func (m *Module) NewEffectCommandFloat(fn func(img image.Image, amount float64) *image.RGBA) func(ctx *system.Context) {
+func (m *Module) NewEffectCommandFloat(fn func(img image.Image, amount float64) *image.RGBA, opts ...EffectOptions) func(ctx *system.Context) {
 	return func(ctx *system.Context) {
 		if ctx.Args.Get(0) == "" {
 			ctx.ReplyError("Please supply a float")
@@ -44,13 +51,24 @@ func (m *Module) NewEffectCommandFloat(fn func(img image.Image, amount float64) 
 			ctx.ReplyError("Error parsing float")
 			return
 		}
+
+		if len(opts) != 0 {
+			o := opts[0]
+			if o.ConstrainMax && amount > o.Max {
+				amount = o.Max
+			}
+			if o.ConstrainMin && amount < o.Min {
+				amount = o.Min
+			}
+		}
+
 		ctx.Reply(amount)
 		ReplyImage(ctx, fn(images[0], amount))
 	}
 }
 
 // NewEffectCommandInt ...
-func (m *Module) NewEffectCommandInt(fn func(img image.Image, amount int) *image.RGBA) func(ctx *system.Context) {
+func (m *Module) NewEffectCommandInt(fn func(img image.Image, amount int) *image.RGBA, opts ...EffectOptions) func(ctx *system.Context) {
 	return func(ctx *system.Context) {
 		if ctx.Args.Get(0) == "" {
 			ctx.ReplyError("Please supply an integer")
@@ -70,6 +88,16 @@ func (m *Module) NewEffectCommandInt(fn func(img image.Image, amount int) *image
 		amount, err := strconv.Atoi(ctx.Args.Get(0))
 		if err != nil {
 			ctx.ReplyError("error parsing integer", err)
+		}
+
+		if len(opts) != 0 {
+			o := opts[0]
+			if o.ConstrainMax && amount > int(o.Max) {
+				amount = int(o.Max)
+			}
+			if o.ConstrainMin && amount < int(o.Min) {
+				amount = int(o.Min)
+			}
 		}
 
 		ReplyImage(ctx, fn(images[0], amount))
