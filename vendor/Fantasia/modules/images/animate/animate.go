780ace4df257e9d3efbc38cbd77d1aa566da5f7a
necro <necroforger@gmail.com>
2018-04-19 02:17:20 -0400

    Added animate options

--- vendor/Fantasia/modules/images/animate/animate.go
+++ vendor/Fantasia/modules/images/animate/animate.go
@@ -19,24 +19,45 @@ var FloydSteinberg = [][]float32{
 	[]float32{1.0 / 48.0, 3.0 / 48.0, 5.0 / 48.0, 3.0 / 48.0, 1.0 / 48.0},
 }
 
+// Options are animation options
+type Options struct {
+	From, To, Increment float64
+
+	// Delay of each frame
+	Delay int
+
+	// Use dithering
+	Dither bool
+
+	// Loop backwards at the end of the loop
+	LoopBackwards bool
+
+	// StopFor is the delay to stop for before beginning the next loop
+	StopFor int
+}
+
 // Animate creates a GIF animation from an Effect
-func Animate(src image.Image, fn Effect, from, to, increment float64, delay int) *gif.GIF {
+func Animate(src image.Image, fn Effect, opts *Options) *gif.GIF {
+	if opts == nil {
+		opts = &Options{}
+	}
+
 	// Set the minimum delay for gifs at 10 ms
-	if delay < 0 {
-		delay = 0
+	if opts.Delay < 0 {
+		opts.Delay = 0
 	}
 
 	var reverse bool
 
-	if from > to {
-		from, to = to, from
+	if opts.From > opts.To {
+		opts.From, opts.To = opts.To, opts.From
 		reverse = true
 	}
-	if increment < 0 {
-		increment = -increment
+	if opts.Increment < 0 {
+		opts.Increment = -opts.Increment
 	}
 
-	gsize := int((to - from) / increment)
+	gsize := int(((opts.To - opts.From) / opts.Increment) + 0.5) // Round up
 	g := gif.GIF{
 		Image:    make([]*image.Paletted, gsize),
 		Delay:    make([]int, gsize),
@@ -45,7 +66,10 @@ func Animate(src image.Image, fn Effect, from, to, increment float64, delay int)
 
 	// Set default delays
 	for i := 0; i < len(g.Delay); i++ {
-		g.Delay[i] = delay
+		g.Delay[i] = opts.Delay
+	}
+	if opts.StopFor > 0 {
+		g.Delay[gsize-1] = opts.StopFor
 	}
 
 	// Set disposal
@@ -62,21 +86,36 @@ func Animate(src image.Image, fn Effect, from, to, increment float64, delay int)
 	}
 
 	wg.Add(gsize)
-	for i := from; i < to; i += increment {
+	for i := opts.From; i < opts.To; i += opts.Increment {
 		<-tokens
 		go func(i float64) {
-			dst := image.NewPaletted(src.Bounds(), nil)
 			mod := fn(src, i)
-			dst.Palette = colorquant.Quant{}.Quantize(mod, 256).(*image.Paletted).Palette
-			colorquant.Dither{Filter: FloydSteinberg}.Quantize(mod, dst, 256, true, true)
 
-			g.Image[int(i/increment)] = dst
+			if opts.Dither {
+				dst := image.NewPaletted(src.Bounds(), nil)
+				dst.Palette = colorquant.Quant{}.Quantize(mod, 256).(*image.Paletted).Palette
+				colorquant.Dither{Filter: FloydSteinberg}.Quantize(mod, dst, 256, true, true)
+				g.Image[int(i/opts.Increment)] = dst
+			} else {
+				g.Image[int(i/opts.Increment)] = colorquant.Quant{}.Quantize(mod, 256).(*image.Paletted)
+			}
+
 			tokens <- struct{}{}
 			wg.Done()
 		}(i)
 	}
-
 	wg.Wait()
+
+	if opts.LoopBackwards {
+		tmp := make([]*image.Paletted, gsize)
+		copy(tmp, g.Image)
+		for i, j := 0, len(tmp)-1; i < j; i, j = i+1, j-1 {
+			tmp[i], tmp[j] = tmp[j], tmp[i]
+		}
+		g.Image = append(tmp, g.Image...)
+		g.Delay = append(g.Delay, g.Delay...)
+		g.Disposal = append(g.Disposal, g.Disposal...)
+	}
 
 	if reverse {
 		for i, j := 0, len(g.Image)-1; i < j; i, j = i+1, j-1 {
